149997e68b179e5375b8083cd8207921814676e1
kathBD <felicidadkg@gmail.com>
2025-01-10 20:39:35 -0500

    variables

---
+++ src/Hi.java
@@ -0,0 +1,7 @@
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is kath");
+        System.out.println("Age: 34");
+        System.out.println("Pais: Colombia");
+    }
+}
