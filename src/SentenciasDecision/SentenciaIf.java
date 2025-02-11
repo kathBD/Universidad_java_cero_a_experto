1335b2aa5200d5aea25a2e80ebb2d274fe23a235
kathBD <felicidadkg@gmail.com>
2025-02-10 19:33:57 -0500

    sentecia if y else if

---
+++ src/SentenciasDecision/SentenciaIf.java
@@ -0,0 +1,26 @@
+package SentenciasDecision;
+
+public class SentenciaIf {
+    public static void main(String[] args) {
+        var edad = 30;
+        if(edad >= 18){
+            System.out.println("Eres mayor de edad: Puedes acceder" );
+        }else {
+            System.out.println("Eres menor de edad: no tienes acceso");
+        }
+
+        //Else if
+        Integer age = 10;
+       
+        if(age <=12) {
+            System.out.println("eres un infante");
+        } else if (age> 13 && age <= 18 ) {
+            System.out.println("Eres un adolescente");
+        }else {
+            System.out.println("Eres mayor de edad");
+        }
+            
+        }
+    }
+
+
