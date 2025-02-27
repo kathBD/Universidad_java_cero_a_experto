020adbb573a6de2c7730bf7740b0be493778a5da
kathBD <felicidadkg@gmail.com>
2025-02-26 19:50:33 -0500

    Ejercicio calculadora

---
+++ src/ciclos/Calculadora.java
@@ -0,0 +1,54 @@
+package ciclos;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("***Calculadora en Java***");
+        Scanner scanner = new Scanner(System.in);
+        Boolean salir = false;
+        int operacion;
+
+        while (!salir) {
+            System.out.println("\nSeleccione una operación:");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicación");
+            System.out.println("4. División");
+            System.out.println("5. Salir");
+            System.out.print("Opción: ");
+            operacion = scanner.nextInt();
+
+
+            double num1, num2;
+
+            System.out.print("Ingrese el primer número: ");
+            num1 = scanner.nextDouble();
+
+            System.out.print("Ingrese el segundo número: ");
+            num2 = scanner.nextDouble();
+
+            switch (operacion) {
+                case 1 -> System.out.println("La suma de " + num1 + " y " + num2 + " es: " + (num1 + num2));
+                case 2 -> System.out.println("La resta de " + num1 + " y " + num2 + " es: " + (num1 - num2));
+                case 3 -> System.out.println("La multiplicación de " + num1 + " y " + num2 + " es: " + (num1 * num2));
+                case 4 -> {
+                    if (num2 != 0)
+                        System.out.println("La división de " + num1 + " y " + num2 + " es: " + (num1 / num2));
+                }case 5 -> {
+                        System.out.print("Saliendo...\s");
+                        salir = true;
+                    }
+                    default -> System.out.println("Opción inválida");
+                }
+
+
+            }
+
+        System.out.println("Fin del sistema");
+        }
+
+    }
+
+
+
