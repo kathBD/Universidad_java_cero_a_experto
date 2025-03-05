c856b7dd9aa8ac7f4893e8bb2e893b2ca73cd638
kathBD <felicidadkg@gmail.com>
2025-03-05 16:32:33 -0500

    Ejercicio calculadora corregido error menú

--- src/ciclos/Calculadora.java
+++ src/ciclos/Calculadora.java
@@ -9,6 +9,10 @@ public class Calculadora {
         Boolean salir = false;
         int operacion;
 
+
+        double num1 =0, num2 = 0;
+        double resultado; //forma del profesor
+
         while (!salir) {
             System.out.println("\nSeleccione una operación:");
             System.out.println("1. Suma");
@@ -18,24 +22,25 @@ public class Calculadora {
             System.out.println("5. Salir");
             System.out.print("Opción: ");
             operacion = scanner.nextInt();
+            if(operacion >= 1 && operacion <=4) {
 
+                System.out.print("Ingrese el primer número: ");
+                num1 = scanner.nextDouble();
 
-            double num1, num2;
-
-            System.out.print("Ingrese el primer número: ");
-            num1 = scanner.nextDouble();
-
-            System.out.print("Ingrese el segundo número: ");
-            num2 = scanner.nextDouble();
-
+                System.out.print("Ingrese el segundo número: ");
+                num2 = scanner.nextDouble();
+            }
             switch (operacion) {
-                case 1 -> System.out.println("La suma de " + num1 + " y " + num2 + " es: " + (num1 + num2));
+                case 1 -> {
+                    resultado = num1 + num2;
+                    System.out.printf("El resultado de la suma: %.2f%n%n", resultado);
+                }
                 case 2 -> System.out.println("La resta de " + num1 + " y " + num2 + " es: " + (num1 - num2));
                 case 3 -> System.out.println("La multiplicación de " + num1 + " y " + num2 + " es: " + (num1 * num2));
                 case 4 -> {
                     if (num2 != 0)
-                        System.out.println("La división de " + num1 + " y " + num2 + " es: " + (num1 / num2));
-                }case 5 -> {
+                        System.out.println("La división de " + num1 + " y " + num2 + " es: " + (num1 / num2)); }
+                case 5 -> {
                         System.out.print("Saliendo...\s");
                         salir = true;
                     }
